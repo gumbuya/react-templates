3d715129e46885b6db3bcc2b97e306eb937b41b7
Leland Lo <lcherng@ascensionlab.com>
2016-01-04 11:50:42 +0800

    try again to put dist folder

---
+++ dist/dist/stringUtils.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _ = require('lodash');
+
+/**
+ * @param {Array.<*>} array
+ * @param {*} obj
+ */
+function addIfMissing(array, obj) {
+    if (!_.includes(array, obj)) {
+        array.push(obj);
+    }
+}
+
+module.exports = { addIfMissing };
